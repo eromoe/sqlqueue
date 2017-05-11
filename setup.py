1f2e963c287ab5fb4d26aef4c31abc62244264b5
eromoe <eromoe@users.noreply.github.com>
2017-05-11 15:22:47 +0800

    fix setup for pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # @Author: mithril
 # @Date:   2016-01-20 09:22:44
 # @Last Modified by:   mithril
-# @Last Modified time: 2017-05-10 09:08:39
+# @Last Modified time: 2017-05-11 15:20:25
 
 from __future__ import unicode_literals, print_function, absolute_import
 import sys
@@ -22,15 +22,16 @@ install_requires = [
 
 setup(
     name='sqlqueue',
-    version='0.1',
+    version='1.0.1',
 
     description='A simple queue lib base on sqlite3',
     long_description=long_description,
 
     author='Mithril',
+    author_email='eromoe@users.noreply.github.com',
 
     classifiers=[
-        'Development Status :: 1 - Beta',
+        'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
@@ -41,16 +42,14 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
 
-        "License :: GPLv3",
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
         'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Task Queue',
+        'Topic :: Software Development :: Object Brokering',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 
-    keywords='sql sqlite3 local disk queue',
-
-
+    keywords='sql sqlite3 local file queue',
     install_requires=install_requires,
 
     py_modules=['sqlqueue'],
