3cb9168991992ba2bf5d187b971d5f4b085b1f5f
eromoe <eromoe@users.noreply.github.com>
2017-12-28 09:35:46 +0800

    update

--- setup.py
+++ setup.py
@@ -3,32 +3,30 @@
 # @Author: mithril
 # @Date:   2016-01-20 09:22:44
 # @Last Modified by:   mithril
-# @Last Modified time: 2017-05-11 15:20:25
+# @Last Modified time: 2017-07-26 15:18:11
 
 from __future__ import unicode_literals, print_function, absolute_import
 import sys
-from setuptools import setup, find_packages
-from codecs import open
 from os import path
+from codecs import open
+from setuptools import setup, find_packages
+
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.md'), "r", encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-
-
-install_requires = [
-]
 
 
 setup(
     name='sqlqueue',
-    version='1.0.1',
+    version='1.0.2',
 
     description='A simple queue lib base on sqlite3',
     long_description=long_description,
 
     author='Mithril',
     author_email='eromoe@users.noreply.github.com',
+    url = 'https://github.com/eromoe/sqlqueue',
 
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -48,10 +46,8 @@ setup(
         'Topic :: Software Development :: Object Brokering',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-
-    keywords='sql sqlite3 local file queue',
+    keywords=['sql','sqlite3','local','file', 'queue'],
     install_requires=install_requires,
-
     py_modules=['sqlqueue'],
 
 )
